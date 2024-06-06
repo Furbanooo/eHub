391d2a4c86a981178b5d42a2a901e3b4615cdb50
Furbanooo <furbandgbaguidi@gmail.com>
2024-06-06 15:51:25 +0200

    implimentation of the views

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import home from '../views/home.vue';
 import Products from '../views/Products.vue';
 import TradeIn from '../views/TradeIn.vue';
 import Recycling from '../views/Recycling.vue';
 import Profile from '../views/Profile.vue';
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
+  { path: '/', name: 'home', component: home },
   { path: '/products', name: 'Products', component: Products },
   { path: '/trade-in', name: 'TradeIn', component: TradeIn },
   { path: '/recycling', name: 'Recycling', component: Recycling },
---
+++ client/src/views/TradeIn.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Trade In</h1>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: 'TradeIn'
+  }
+  </script>
+  <style scoped>
+  h1 {
+    color: #333;
+  }
+  </style>
+  
